1d5a438ad0e38c4bccddd31ad5efbe9c1fe90528
Nameless <nameless@thenamelessdev.com>
2026-05-11 13:31:14 +0200

    feat: add command type and tokenizer

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import "strings"
+
+type Command struct {
+	Name string   `json:"name"`
+	Args []string `json:"args"`
+}
+
+func tokenize(line string) []string {
+	var tokens []string
+
+	tokens = strings.Split(line, " ")
+	return tokens;
+}
+
 func main() {
-	
+
 }
